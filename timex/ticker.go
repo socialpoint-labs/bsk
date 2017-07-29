501d5e2bfbab3a0f54583341fc79cebda2dbb383
Roger Clotet <clotet89@gmail.com>
2017-07-29 19:49:43 +0200

    Use runner in ticker (#1)

--- timex/ticker.go
+++ timex/ticker.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"time"
 
-	"github.com/socialpoint-labs/bsk/server"
+	"github.com/socialpoint-labs/bsk/runner"
 )
 
 // RunInterval runs the provided function at intervals specified by the interval argument.
@@ -25,9 +25,9 @@ func RunInterval(ctx context.Context, interval time.Duration, f func()) {
 	}
 }
 
-// IntervalRunner returns a server.Runner that runs the function RunInterval with the provided context
-func IntervalRunner(interval time.Duration, f func()) server.Runner {
-	return server.RunnerFunc(func(ctx context.Context) {
+// IntervalRunner returns a runner.Runner that runs the function RunInterval with the provided context
+func IntervalRunner(interval time.Duration, f func()) runner.Runner {
+	return runner.RunnerFunc(func(ctx context.Context) {
 		RunInterval(ctx, interval, f)
 	})
 }
