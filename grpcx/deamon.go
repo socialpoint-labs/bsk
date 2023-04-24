623cc469182d0510af1d2707a6a1e352b6d044e4
Jordi Forns <jordi.forns@socialpoint.es>
2023-04-24 12:03:08 +0200

    Add constructors to pass port or an actual Listener; removed panics

--- grpcx/deamon.go
+++ grpcx/deamon.go
@@ -13,7 +13,9 @@ import (
 // Daemon represents a runnable gRPC daemon
 type Daemon struct {
 	port int
+	lis  net.Listener
 	svr  *grpc.Server
+	ef   func(error)
 	apps []Application
 }
 
@@ -23,16 +25,31 @@ type Application interface {
 	RegisterGRPC(grpc.ServiceRegistrar)
 }
 
-func NewDaemon(port int, svr *grpc.Server, apps ...Application) Daemon {
+// NewDaemonWithPort creates a gRPC Daemon listening at the given port.
+func NewDaemonWithPort(port int, svr *grpc.Server, ef func(error), apps ...Application) Daemon {
 	return Daemon{
 		port: port,
 		svr:  svr,
+		ef:   ef,
 		apps: apps,
 	}
 }
 
-// Run registers and runs every application of the Daemon with a gRPC server,
-// then starts it and gracefully shuts it down once the context is done.
+// NewDaemon creates a gRPC Daemon for the given listener.
+//
+// The lifecycle of the listener will *not* be managed by this function, nor by the Daemon. The application
+// needs to take manage it.
+func NewDaemon(lis net.Listener, svr *grpc.Server, ef func(error), apps ...Application) Daemon {
+	return Daemon{
+		lis:  lis,
+		svr:  svr,
+		ef:   ef,
+		apps: apps,
+	}
+}
+
+// Run registers every application of the Daemon with a gRPC server and runs them,
+// then starts the server and gracefully shuts it down once the context is done.
 func (d Daemon) Run(ctx context.Context) {
 	for _, app := range d.apps {
 		app.RegisterGRPC(d.svr)
@@ -40,18 +57,28 @@ func (d Daemon) Run(ctx context.Context) {
 		go app.Run(ctx)
 	}
 
-	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", d.port))
+	listener, err := d.listener()
 	if err != nil {
-		panic(err.Error())
+		d.ef(err)
+		return
 	}
 
 	defer d.svr.GracefulStop()
 
 	go func() {
 		if err := d.svr.Serve(listener); err != nil {
-			panic(err.Error())
+			d.ef(err)
+			return
 		}
 	}()
 
 	<-ctx.Done()
+}
+
+func (d Daemon) listener() (net.Listener, error) {
+	if d.lis != nil {
+		return d.lis, nil
+	}
+
+	return net.Listen("tcp", fmt.Sprintf(":%d", d.port))
 }
